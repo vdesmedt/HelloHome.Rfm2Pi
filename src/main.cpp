f8be2a2fa0ded04c9b106084d552889d54229e81
Valery De Smedt <valery@indri.be>
2018-01-05 09:06:52 +0100

    Improved time display

--- src/main.cpp
+++ src/main.cpp
@@ -39,10 +39,8 @@
 RFM69 radio;
 char gtwBuffer[100];
 byte gtwBufferLength;
-bool receiving = false;
 char serBuffer[100];
 byte serBufferLength;
-bool sending = false;
 byte targetID=0;
 
 //Logging
@@ -55,7 +53,7 @@ unsigned long lastLogMillis = 0;
 int screenLogIndex = 0;
 
 void addToScreenLog(const char * log);
-void draw(void);
+bool draw(void);
 
 void setup() {
   radio.initialize(FREQUENCY,NODEID,NETWORKID);
@@ -76,13 +74,11 @@ void setup() {
 
   ssd1306_128x64_i2c_init();
   ssd1306_fillScreen(0x00);
-  draw();
+  ssd1306_charF6x8(0, 0, netId);
 }
 
 void loop() {
-  receiving = sending = false;
     if (radio.receiveDone()) {
-      receiving = true;
       digitalWrite(LED,HIGH);
       int rssi = radio.RSSI;
       if(radio.DATALEN > 97) {
@@ -107,7 +103,6 @@ void loop() {
     while(Serial.available() > 0) {
       serBuffer[serBufferLength++] = Serial.read();
       if(serBufferLength >=2 && serBuffer[serBufferLength-2] == 13 && serBuffer[serBufferLength-1] == 10) {
-        sending = true;
         digitalWrite(LED, HIGH);
         bool success = radio.sendWithRetry(serBuffer[0], serBuffer+1, serBufferLength-2, 3, 40);
         digitalWrite(LED, LOW);
@@ -116,11 +111,8 @@ void loop() {
         addToScreenLog(screenLogLine);
       }
     }
-    long start = millis();
+
     draw();
-    long stop = millis() - start;
-    Serial.print("Printing took (ms) ");
-    Serial.println(stop);
 }
 
 void addToScreenLog(const char * log) {
@@ -129,21 +121,29 @@ void addToScreenLog(const char * log) {
     screenLogIndex = ++screenLogIndex % LOGSIZE;
 }
 
-void draw(void ) {
-  ssd1306_charF6x8(0, 0, netId);
+unsigned long lastPrint = millis();
+bool draw(void ) {
+  if(millis()-lastPrint < 500)
+    return false;
   int logIndex = 0;
   for(logIndex = 1 ; logIndex <= LOGSIZE ; logIndex++) 
   {
     int i = (screenLogIndex-logIndex+8) % LOGSIZE;
-    unsigned int s = (millis()-screenLogTimes[i])/1000;
-    if(s < 5 * 60)
-      snprintf(screenLogLine, 20, "%3is", s);
-    else if (s < 120 * 60)
-      snprintf(screenLogLine, 20, "%3im", s/60);
-    else
-      snprintf(screenLogLine, 20, "%3ih", s/3600);
+    unsigned long s = (millis()-screenLogTimes[i])/1000;
+    unsigned int sec = s % 60L;
+    unsigned int min = (s / 60L) % 60L;
+    unsigned int hou = (s % 86400L) / 3600L;
+    
+    if(hou > 0) {
+      snprintf(screenLogLine, 20, "%02i:%02i", hou, min);
+    }
+    else {
+      snprintf(screenLogLine, 20, "%02i:%02i", min, sec);
+    }
 
     ssd1306_charF6x8(0, logIndex+1, screenLogLine);
     ssd1306_charF6x8(35, logIndex+1, screenLog[i]);
   }
+  lastPrint = millis();
+  return true;
 }
